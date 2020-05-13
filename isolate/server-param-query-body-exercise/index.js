ac3b193c86aa1d2987bbd7559a0d5e1d4e6c92f6
mesutBE <gelukkig.zijn.be@gmail.com>
2020-05-13 18:50:42 +0200

    param-query-body needs revision

--- isolate/server-param-query-body-exercise/index.js
+++ isolate/server-param-query-body-exercise/index.js
@@ -2,29 +2,64 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const config = require('./config');
 const logger = require('./middleware/logger');
 
 // initialize the app
-_;
+const app = express();
 
 // log requests
-_;
+app.use(logger);
 
 // parse body
-_;
+app.use(bodyParser.json());
+app.use(bodyParser.raw({ type: 'text/plain' }));
+
 // parse queries
-_;
+// _;
 
 // statically serve the frontend
-_;
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // declare the routes
-_;
 
-_;
+app.post('/body', (req, res) => {
+    const bodyValue = req.body.text;
 
-_;
+    console.log(`body value: ${bodyValue}`);
+
+    const responseData = {
+        bodyValue,
+    };
+    res.json(responseData);
+});
+
+app.post('/param/:paramVal', (req, res) => {
+    const paramValue = req.params.paramVal;
+
+    console.log(`param value: ${paramValue}`);
+
+    const responseData = {
+        paramValue,
+    };
+    res.json(responseData);
+});
+
+app.post('/:queryVal', (req, res) => {
+    const queryValue = req.query.queryVal;
+
+    console.log(`param value: ${queryValue}`);
+
+    const responseData = {
+        queryValue,
+    };
+    res.json(responseData);
+});
 
 // start the app
-_;
+app.listen(config.PORT, () => {
+    console.log(
+        `Example app listening at http://localhost:${config.PORT} (${config.MODE} mode)`
+    );
+});
